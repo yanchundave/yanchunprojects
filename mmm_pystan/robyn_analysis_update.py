fb5c9c6745b8723fb13146a56defe9573f875158
Yanchun Yang <yanchunyang@Yanchun-Yang-mbp.dave.com>
2022-12-15 11:32:58 -0800

    update

--- mmm_pystan/robyn_analysis_update.py
+++ mmm_pystan/robyn_analysis_update.py
@@ -1,5 +1,7 @@
 import pandas as pd
+from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+import numpy as np
 
 analysis_path = "/Users/yanchunyang/Documents/datafiles/Rfile/advance/"
 
@@ -96,6 +98,46 @@ def compare_hist_distribution(hist_spending):
 
     ratio_roi.to_csv(analysis_path + "ratio_roi_hist.csv")
 
+def get_point(minspend, maxspend, a, b, c, d):
+    cumx = minspend
+    cumy = 0
+    x = [0]
+    y = [0]
+    dis = int((maxspend - minspend) / 100)
+    for i in range(0, 100):
+        cumx += dis
+        rate = func1(cumx, a, b, c, d)
+        if rate < 0:
+            print(cumx)
+        cumy += func1(cumx, a, b, c, d) * dis
+        if cumy > 0:
+            x.append(cumx)
+            y.append(cumy)
+    return x[1:], y[1:]
+
+def get_point_update(minspend, maxspend, p):
+    cumx = minspend
+    cumy = 0
+    x = [0]
+    y = [0]
+    dis = int((maxspend - minspend) / 100)
+    for i in range(0, 100):
+        cumx += dis
+        rate = p(cumx)
+        if rate < 0:
+            print(cumx)
+        cumy += rate * dis
+        if cumy > 0:
+            x.append(cumx)
+            y.append(cumy)
+    return x[1:], y[1:]
+
+def get_current_point(x_1, x, y):
+    for i in range(0, len(x)):
+        if x[i] > x_1:
+            return x[i-1], y[i-1]
+    return x_1, 0
+
 
 def draw_roas_rate(tmp_channel):
     real_spending = df.loc[:, ['date', tmp_channel]]
@@ -104,15 +146,70 @@ def draw_roas_rate(tmp_channel):
     spending_response = pd.merge(real_spending, decom, on=['ds'], how='inner')
     spending_response = spending_response.loc[spending_response[tmp_channel+'spending'] > 0,:]
     spending_response['rate'] = spending_response[tmp_channel] / (spending_response[tmp_channel + 'spending'])
-    spending_response.loc[spending_response['rate'] <10, :]
+    spending_response = spending_response.loc[spending_response['rate'] <4, :]
+    # get break even point
     filtered_spending = spending_response.loc[(spending_response['rate'] >=0.9) & (spending_response['rate'] <= 1.1), :]
-    spending_needed = filtered_spending[tmp_channel+'spending'].mean()
-    print(spending_needed)
-    spending_response = spending_response.sort_values(by=[tmp_channel + 'spending'])
-    plt.plot(spending_response[tmp_channel+'spending'], spending_response['rate'])
-    plt.savefig(analysis_path + tmp_channel + '_rate.png')
-    return spending_needed
 
+    spending_needed = filtered_spending[tmp_channel+'spending'].mean()
+    #print(spending_needed)
+    spending_response = spending_response.sort_values(by=[tmp_channel + 'spending'])
+    """
+    params, _ = curve_fit(func1, spending_response[tmp_channel+'spending'], spending_response['rate'])
+    a, b, c, d= params[0], params[1], params[2], params[3]
+    maxspend = spending_response[tmp_channel+'spending'].max()
+    minspend = spending_response[tmp_channel+'spending'].min()
+    x, y = get_point(minspend, maxspend, a, b, c, d)
+    yfit1 = func1(spending_response[tmp_channel+'spending'], a, b, c, d)
+    #ypredict = spending_response[tmp_channel+'spending'] * yfit1
+    """
+    p = func2(spending_response[tmp_channel+'spending'], spending_response['rate'], 7)
+    yhat = p(spending_response[tmp_channel+'spending'])
+
+    maxspend = spending_response[tmp_channel+'spending'].max()
+    minspend = spending_response[tmp_channel+'spending'].min()
+    x, y = get_point_update(minspend, maxspend, p)
+
+    #yfit1 = func1(spending_response[tmp_channel+'spending'], a, b, c, d)
+    #plt.plot(spending_response[tmp_channel+'spending'], spending_response['rate'])
+    #plt.plot(spending_response[tmp_channel+'spending'], ypredict)
+    #plt.plot(spending_response[tmp_channel+'spending'], yhat)
+    #plt.savefig(analysis_path + tmp_channel + '_rate.png')
+    #plt.close()
+    #return spending_needed
+    x_0 = metrics.loc[metrics['rn'] == tmp_channel, 'mean_spend'].values[0]
+    x_1, y_1 = get_current_point(x_0, x, y)
+    return x, y, x_1, y_1
+
+def func1(x, a, b, c, d):
+    return a*x**3+ b*x**2 + c*x + d
+
+def func2(x, y, degree):
+    coeffs = np.polyfit(x, y, degree)
+    p = np.poly1d(coeffs)
+    return p
+
+def draw_saturtion_customized(channel_list):
+    title = ""
+    xt =[]
+    yt =[]
+    xs = []
+    ys = []
+    for item in channel_list:
+        title += item + ", "
+        x, y, x_1, y_1 = draw_roas_rate(item)
+        xt.append(x)
+        yt.append(y)
+        xs.append((x_1, y_1))
+        plt.plot(xt[-1], yt[-1],label=item, linewidth=1)
+        #plt.plot(xy[item+'_x'], yfit1, label=item, linewidth=1, color='green')
+
+        plt.scatter(x_1, y_1)
+    plt.legend()
+    plt.title("Saturation Curves of " + title)
+    plt.gcf().set_size_inches(10, 5)
+    plt.style.use('fast')
+    plt.savefig(analysis_path + title.strip()+ ".png")
+    plt.close()
 
 def draw_saturation(channel_list):
     spending = transform_file.loc[(transform_file['type']== 'adstockedMedia')&(transform_file['solID'] == solID),:]
@@ -124,9 +221,16 @@ def draw_saturation(channel_list):
         y = response.loc[:,['ds',item]]
         xy = pd.merge(x, y, on=['ds'])
         xy = xy.sort_values(by=[item+'_x'])
+
+        x_1 = spending[item].mean()
+        y_1 = response[item].mean()
+        """
         x_1 = metrics.loc[metrics['rn'] == item, 'mean_spend'].values[0]
         y_1 = metrics.loc[metrics['rn'] == item, 'mean_response'].values[0]
+        """
         plt.plot(xy[item+'_x'], xy[item+'_y'], label=item, linewidth=1)
+        #plt.plot(xy[item+'_x'], yfit1, label=item, linewidth=1, color='green')
+
         plt.scatter(x_1, y_1)
     plt.legend()
     plt.title("Saturation Curves of " + title)
@@ -140,12 +244,28 @@ def main():
 
     #compare_optimal_distribution(optimal_spending)
     #compare_hist_distribution(hist_spending)
+    """
     spending_threshold = {}
     for channel in colname:
         spending = draw_roas_rate(channel)
         spending_threshold[channel] = spending
     print(spending_threshold)
+    """
+    #channel_list = ['Apple_Search_Ads_iOS', 'Adwords_iOS', 'Facebook_Android']
+    #channel_list = ['Snapchat_iOS', 'bytedanceglobal_int_Android','bytedanceglobal_int_iOS']
+    #channel_list = [ 'Tatari_TV', 'Facebook_iOS', 'Adwords_Android',]
+    channel_list = [ 'new_channels']
+    """
+    for item in colname:
+        print(item)
+        draw_saturtion_customized([item])
+    """
+    draw_saturtion_customized(colname)
 
+    #draw_roas_rate('Apple_Search_Ads_iOS')
+    #draw_roas_rate('Adwords_iOS')
+    #for item in colname:
+    #    draw_roas_rate(item)
 
 
 if __name__ == '__main__':
