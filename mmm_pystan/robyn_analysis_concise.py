e472b50f7bd823c897fff1cf52c569b0f20adc1b
Yanchun Yang <yanchunyang@Yanchun-Yang-mbp.dave.com>
2023-01-01 17:25:16 -0800

    update

--- mmm_pystan/robyn_analysis_concise.py
+++ mmm_pystan/robyn_analysis_concise.py
@@ -6,8 +6,10 @@ from collections import defaultdict
 import math
 import hillfit
 
-analysis_path = "/Users/yanchunyang/Documents/datafiles/Rfile/advance/"
-solID = '5_200_2'
+analysis_path_origin = "/Users/yanchunyang/Documents/datafiles/Rfile/advance/"
+customized_folder = "20230101/"
+analysis_path = analysis_path_origin + customized_folder
+solID = '3_198_5'
 optimal_spending_file = solID + "_reallocated_optimal.csv"
 hist_spending_file = solID + "_reallocated_hist.csv"
 
@@ -21,14 +23,14 @@ transform_file = pd.read_csv(analysis_path + "pareto_media_transform_matrix.csv"
 # model summary metrics
 metrics = pd.read_csv(analysis_path + "R_metrics_advance_1.csv")
 # optimal spending suggestion and historic spending suggestion
-optimal_spending = pd.read_csv(analysis_path + optimal_spending_file)
-hist_spending = pd.read_csv(analysis_path + hist_spending_file)
+#optimal_spending = pd.read_csv(analysis_path + optimal_spending_file)
+#hist_spending = pd.read_csv(analysis_path + hist_spending_file)
 
 colname = [
     'Snapchat_Android', 'bytedanceglobal_int_Android',
     'bytedanceglobal_int_iOS', 'Snapchat_iOS', 'Adwords_iOS',
     'Apple_Search_Ads_iOS', 'Tatari_TV', 'Facebook_Android', 'Facebook_iOS',
-    'Adwords_Android',  'new_channels'
+    'Adwords_Android',  'new_channels', 'minor_channels'
 ]
 
 nextspending = 11000000
@@ -38,11 +40,8 @@ historic_time_span = 295
 # Robyn mean_spending from metrics are the average mean spending without considering dates without spending
 # We have to estimate the ROAS by decompse value
 # need figure out a better way to estimate ROAS through smoothing technique, but temporarily solve by the below function
-
-
-def hillfunc(x, a, b, c, d):
-    y = a + (b-a)/(1 + math.power(c/x, d))
-    return y
+saturation_dic = {}
+original_dic = {}
 
 def calculate_margin(tmp_channel, mean_spending):
     # this spending is adstorkedMedia
@@ -70,7 +69,7 @@ def calculate_margin(tmp_channel, mean_spending):
     margin = sum(tupdate[tmp_channel]) / (sum(tupdate[spendingchannelname])+0.01)
     return margin
 
-def compare_optimal_distribution(optimal_spending):
+def compare_optimal_distribution_optimal(optimal_spending):
     ratio = optimal_spending.loc[optimal_spending['solID'] == solID, ['channels', 'initSpendShare', 'optmSpendShareUnit']]
     ratio.columns = ['rn', 'init_rate', 'suggest_rate']
     ratio_roi = pd.merge(ratio, metrics.loc[:, ['rn', 'roi_total', 'total_spend', 'total_response', 'mean_spend']], on=['rn'])
@@ -87,10 +86,10 @@ def compare_optimal_distribution(optimal_spending):
     ratio_roi['init_daily_response'] = ratio_roi['init_roi'] * ratio_roi['init_daily']
     ratio_roi['suggest_daily_response'] = ratio_roi['suggest_roi'] * ratio_roi['suggest_daily']
 
-    ratio_roi.to_csv(analysis_path + "ratio_roi.csv")
+    ratio_roi.to_csv(analysis_path + "ratio_roi_optimal.csv")
 
-def compare_hist_distribution(hist_spending):
-    ratio = hist_spending.loc[hist_spending['solID'] == '5_200_2',
+def compare_hist_distribution_optimal(hist_spending):
+    ratio = hist_spending.loc[hist_spending['solID'] == solID,
                                  ['channels', 'initSpendShare', 'optmSpendShareUnit']]
     ratio.columns = ['rn', 'init_rate', 'suggest_rate']
     ratio_roi = pd.merge(ratio, metrics.loc[:, ['rn', 'mean_spend','roi_total', 'total_spend', 'total_response']], on=['rn'])
@@ -106,6 +105,55 @@ def compare_hist_distribution(hist_spending):
     ratio_roi['init_response_total'] = ratio_roi['init_roi'] * ratio_roi['total_spend']
     ratio_roi['suggest_response_total'] = ratio_roi['suggest_roi'] * ratio_roi['suggest_spending']
 
+    ratio_roi.to_csv(analysis_path + "ratio_roi_hist_optimal.csv")
+
+
+def calculate_margin_update(row):
+    tmp_channel = row['rn']
+    mean_spending_init = row['init_daily']
+    mean_spending_suggest = row['suggest_daily']
+
+    points = saturation_dic[tmp_channel]
+    x_1, y_1, x_2, y_2 = get_current_point(mean_spending_init, points[0], points[1])
+    row['init_response'] = y_1
+    row['init_margin'] = (y_2 - y_1)/(x_2 - x_1 + 0.01)
+
+    x_1, y_1, x_2, y_2 = get_current_point(mean_spending_suggest, points[0], points[1])
+    row['suggest_response'] = y_1
+    row['suggest_margin'] = (y_2 - y_1)/(x_2 - x_1 + 0.01)
+
+    return row
+
+def compare_optimal_distribution(optimal_spending):
+
+    ratio = optimal_spending.loc[optimal_spending['solID'] == solID, ['channels', 'initSpendShare', 'optmSpendShareUnit']]
+    ratio.columns = ['rn', 'init_rate', 'suggest_rate']
+    ratio_roi = pd.merge(ratio, metrics.loc[:, ['rn', 'roi_total', 'total_spend', 'total_response', 'mean_spend']], on=['rn'])
+
+    ratio_roi['init_spending'] = ratio_roi['init_rate'] * nextspending
+    ratio_roi['suggest_spending'] = ratio_roi['suggest_rate'] * nextspending
+
+    ratio_roi['init_daily'] = ratio_roi['init_spending'] / nextday
+    ratio_roi['suggest_daily'] = ratio_roi['suggest_spending']/ nextday
+
+    ratio_roi = ratio_roi.apply(lambda x: calculate_margin_update(x), axis=1)
+
+    ratio_roi.to_csv(analysis_path + "ratio_roi.csv")
+
+def compare_hist_distribution(hist_spending):
+
+    ratio = hist_spending.loc[hist_spending['solID'] == solID,
+                                 ['channels', 'initSpendShare', 'optmSpendShareUnit']]
+    ratio.columns = ['rn', 'init_rate', 'suggest_rate']
+    ratio_roi = pd.merge(ratio, metrics.loc[:, ['rn', 'mean_spend','roi_total', 'total_spend', 'total_response']], on=['rn'])
+    spending = sum(ratio_roi['total_spend'])
+
+    ratio_roi['suggest_spending'] = ratio_roi['suggest_rate'] * spending
+    ratio_roi['suggest_daily'] = ratio_roi['suggest_spending'] / historic_time_span
+    ratio_roi['init_daily'] = ratio_roi['mean_spend']
+
+    ratio_roi = ratio_roi.apply(lambda x: calculate_margin_update(x), axis=1)
+
     ratio_roi.to_csv(analysis_path + "ratio_roi_hist.csv")
 
 
@@ -116,7 +164,7 @@ def get_current_point(x_1, x, y):
     return x_1, 0, 1, 0
 
 
-def draw_spending_response(tmp_channel):
+def get_spending_response(tmp_channel):
     real_spending = df.loc[:, ['date', tmp_channel]]
     real_spending.columns = ['ds',tmp_channel + 'spending']
     decom = decompose.loc[decompose['solID']== solID, ['ds', tmp_channel]]
@@ -126,21 +174,13 @@ def draw_spending_response(tmp_channel):
     spending_response['rate'] = spending_response[tmp_channel] / (spending_response[tmp_channel + 'spending'])
     spending_response = spending_response.loc[spending_response['rate'] <4, :]
     spending_response = spending_response.sort_values(by=[tmp_channel + 'spending'])
-    p = func2(spending_response[tmp_channel+'spending'], spending_response[tmp_channel], 2)
-    spending_response['yhat'] = p(spending_response[tmp_channel + 'spending'])
-    hf = hillfit.HillFit(spending_response[tmp_channel + 'spending'], spending_response['yhat'])
-    hf.fitting(x_label='x', y_label='y', title='Fitted Hill equation', sigfigs=6, log_x=False, print_r_sqr=True,
-           generate_figure=False, view_figure=True, export_directory=None, export_name=None)
-    x_0 = metrics.loc[metrics['rn'] == tmp_channel, 'mean_spend'].values[0]
-    x_1, y_1, x_2, y_2 = get_current_point(x_0, hf.x_fit, hf.y_fit)
-    #plt.plot(spending_response[tmp_channel + 'spending'], spending_response[tmp_channel])
-    return hf.x_fit, hf.y_fit, x_1, y_1, x_2, y_2
 
-# For better smoothing result
-def func2(x, y, degree):
-    coeffs = np.polyfit(x, y, degree)
-    p = np.poly1d(coeffs)
-    return p
+    hf = hillfit.HillFit(spending_response[tmp_channel + 'spending'], spending_response[tmp_channel])
+    hf.fitting(x_label='x', y_label='y', title='Fitted Hill equation', sigfigs=6, log_x=False, print_r_sqr=True,
+           generate_figure=False, view_figure=False, export_directory=None, export_name=None)
+
+    return hf.x_fit, hf.y_fit, spending_response[tmp_channel + 'spending'], spending_response[tmp_channel]
+
 
 # This is the customized saturation curve which used in calculation
 def draw_saturtion_customized(channel_list):
@@ -148,9 +188,11 @@ def draw_saturtion_customized(channel_list):
     margin_dic = {}
     for item in channel_list:
         title += item + ", "
-        x, y, x_1, y_1, x_2, y_2 = draw_spending_response(item)
+        x_0 = metrics.loc[metrics['rn'] == item, 'mean_spend'].values[0]
+        x_1, y_1, x_2, y_2 = get_current_point(x_0, saturation_dic[item][0], saturation_dic[item][1])
         margin = (y_2 - y_1)/(x_2 - x_1 + 0.01)
-        plt.plot(x, y,label=item, linewidth=1)
+        plt.plot(saturation_dic[item][0], saturation_dic[item][1],label=item, linewidth=1)
+        #plt.plot(x_orig, y_orig, color='blue')
         plt.scatter(x_1, y_1)
         margin_dic[item] = margin
     plt.legend()
@@ -175,12 +217,8 @@ def draw_saturation(channel_list):
 
         x_1 = spending[item].mean()
         y_1 = response[item].mean()
-        """
-        x_1 = metrics.loc[metrics['rn'] == item, 'mean_spend'].values[0]
-        y_1 = metrics.loc[metrics['rn'] == item, 'mean_response'].values[0]
-        """
+
         plt.plot(xy[item+'_x'], xy[item+'_y'], label=item, linewidth=1)
-        #plt.plot(xy[item+'_x'], yfit1, label=item, linewidth=1, color='green')
 
         plt.scatter(x_1, y_1)
     plt.legend()
@@ -191,6 +229,11 @@ def draw_saturation(channel_list):
 
 
 def main():
+    for item in colname:
+        x_1, y_1, x_2, y_2 = get_spending_response(item)
+        saturation_dic[item] = [x_1, y_1]
+        original_dic[item] = [x_2, y_2]
+
     optimal_spending = pd.read_csv(analysis_path + solID + "_reallocated_best_1.csv")
     hist_spending = hist_spending = pd.read_csv(analysis_path + solID + "_reallocated_hist.csv")
 
@@ -202,11 +245,11 @@ def main():
 
     #channel_list = ['Apple_Search_Ads_iOS', 'Adwords_iOS', 'Facebook_Android']
     #channel_list = ['Snapchat_iOS', 'bytedanceglobal_int_Android','bytedanceglobal_int_iOS']
-    channel_list = [ 'Tatari_TV', 'Facebook_iOS', 'Adwords_Android',]
+    #channel_list = [ 'Tatari_TV', 'Facebook_iOS', 'Adwords_Android',]
     #channel_list = [ 'new_channels']
     #channel_list = [ 'Tatari_TV', 'new_channels']
     #small_channels = ['Snapchat_iOS', 'bytedanceglobal_int_Android','bytedanceglobal_int_iOS', 'Snapchat_Android']
-    #big_channels = ['Apple_Search_Ads_iOS', 'Adwords_iOS', 'Facebook_Android', 'Facebook_iOS', 'Tatari_TV', 'new_channels', 'Adwords_Android']
+    channel_list = ['Apple_Search_Ads_iOS', 'Adwords_iOS', 'Facebook_Android', 'Facebook_iOS', 'Tatari_TV', 'new_channels', 'Adwords_Android']
 
     draw_saturtion_customized(channel_list)
 
