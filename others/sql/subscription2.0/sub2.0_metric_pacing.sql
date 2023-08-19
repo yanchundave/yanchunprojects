189a1264c4de45749a513171f510ae2c9e566c19
Yanchun Yang <yanchunyang@Yanchun-Yang-mbp.dave.com>
2023-08-18 17:07:50 -0700

    update

---
+++ others/sql/subscription2.0/sub2.0_metric_pacing.sql
@@ -0,0 +1,163 @@
+with recursive_attempt_date as
+(
+    select
+        Date_trunc('month', CURRENT_DATE() - 365) as start_date,
+        current_date() as end_date
+),
+cte_attempt_date(ref_date) as
+(
+    select
+        start_date as ref_date
+        from recursive_attempt_date
+        union all
+        select
+        ref_date + 1 as ref_date
+        from cte_attempt_date
+        cross join
+        recursive_attempt_date
+        where ref_date < end_date
+),
+start_month as
+(
+  select distinct date_trunc('month', ref_date) as start_date from cte_attempt_date
+),
+date_combine as (
+    select
+        a.start_date,
+        b.ref_date as end_date
+    from start_month a
+    join cte_attempt_date b
+    on a.start_date = date_trunc('month', b.ref_date)
+
+),
+
+sub_new as (
+    select * from DAVE.subscription.subscription sub
+),
+sc_charge_new as (
+    select * from DAVE.SUBSCRIPTION.SUBSCRIPTION_CHARGE
+),
+attempts_new as (
+    select * from DAVE.SUBSCRIPTION.subscription_charge_attempt
+),
+sc_status_new as (
+    SELECT * FROM DAVE.SUBSCRIPTION.subscription_charge_status
+),
+tier_new AS
+(
+    SELECT * FROM DAVE.subscription.tier
+),
+payment_method AS
+(
+    SELECT * FROM DAVE.SUBSCRIPTION.PAYMENT_METHOD_KIND
+),
+subscriber_new AS (
+    SELECT
+        sub.USER_ID,
+        sub.ID AS SUBSCRIPTION_ID,
+        tier.ID as tier_id
+    FROM sub_new sub
+    JOIN tier_new tier
+    ON sub.tier_id = tier.id
+    WHERE tier.code = 'one_dollar'
+        AND  sub._DELETED = FALSE
+),
+sub_charge_new AS (
+    SELECT
+        DATE_TRUNC('MONTH',  CONVERT_TIMEZONE('UTC', 'America/Los_Angeles', sc_charge.term_started)) AS billing_cycle,
+        DATE(CONVERT_TIMEZONE('UTC', 'America/Los_Angeles', sc_charge.term_started)) as term_started,
+        subscriber.user_id,
+        sc_charge.id AS subscription_charge_id,
+        sc_status.code as collect_status,
+        payment.kind,
+        sc_charge.unit_cost
+    FROM subscriber_new subscriber
+    JOIN sc_charge_new sc_charge ON subscriber.subscription_id = sc_charge.subscription_id
+    LEFT JOIN sc_status_new sc_status ON sc_charge.subscription_charge_status_id = sc_status.id
+    LEFT JOIN payment_method payment ON sc_charge.payment_method_kind_id = payment.id
+),
+sub_attempts_new AS (
+    SELECT
+        a.id as subscription_attempt_id,
+        s.subscription_charge_id,
+        a.created,
+        DATE(CONVERT_TIMEZONE('UTC', 'America/Los_Angeles', a.CREATED)) AS attempt_date,
+        DATE_TRUNC('month', DATE(CONVERT_TIMEZONE('UTC', 'America/Los_Angeles', a.CREATED))) as monthnum,
+        s.billing_cycle,
+        s.user_id,
+        s.kind as payment_method
+    FROM attempts_new a
+    JOIN sub_charge_new s on a.subscription_charge_id = s.subscription_charge_id
+    WHERE a.created >= s.term_started
+    order by monthnum, attempt_date, created
+),
+attempt_group_new as
+(
+    select
+        billing_cycle,
+        user_id,
+        subscription_charge_id,
+        attempt_date,
+        monthnum,
+        payment_method,
+        count(distinct subscription_attempt_id) as attempt_cnt
+    from sub_attempts_new
+    group by 1, 2, 3, 4, 5, 6
+),
+last_attempt_new as (
+    select
+        subscription_charge_id,
+        date(max(created)) as last_attempt_date
+    from attempts_new
+    group by 1
+),
+success_sub_new as (
+    select
+        sc.billing_cycle,
+        sc.term_started,
+        sc.user_id,
+        sc.subscription_charge_id,
+        sc.collect_status,
+        sc.unit_cost / 100 as unit_cost,
+        la.last_attempt_date
+    from sub_charge_new sc
+    join last_attempt_new la
+    on sc.subscription_charge_id = la.subscription_charge_id
+),
+final_new as (
+    select
+        a.billing_cycle,
+        a.user_id,
+        a.subscription_charge_id,
+        a.attempt_date,
+        a.monthnum,
+        a.attempt_cnt,
+        s.unit_cost,
+        iff(s.collect_status in ('collected', 'pending_collection'), 1, 0) as if_succeed,
+        s.collect_status,
+        a.payment_method
+    from attempt_group_new a
+    left join success_sub_new s
+    on a.subscription_charge_id = s.subscription_charge_id
+    and a.attempt_date = s.last_attempt_date
+),
+sub2 as (
+    select
+      date(date_combine.start_date) as start_date,
+      date(date_combine.end_date) as end_date,
+      date_part('day', date(date_combine.end_date)) as daynum,
+      to_varchar(coalesce(date(final.billing_cycle), DATE_TRUNC('MONTH',  date(date_combine.start_date))), 'YYYY-MM') as billing_cycle,
+      final.payment_method,
+      sum(coalesce(final.attempt_cnt, 0)) as attempt_total_cnt,
+      count(distinct final.user_id) as attempted_user,
+      sum(final.if_succeed) as total_success,
+      sum(final.unit_cost * final.if_succeed) as collect_amount
+    from date_combine
+    left join  final_new final
+    --on final.monthnum = date_combine.start_date and final.attempt_date <= date_combine.end_date
+    on final.attempt_date <= date_combine.end_date
+    group by 1, 2, 3, 4, 5
+    order by 1, 2, 3, 4, 5
+
+)
+select * from sub2
--- others/sql/tmp/dat_2421.sql
+++ others/sql/tmp/dat_2421.sql
@@ -11,8 +11,8 @@ $$
          WHEN LOWER(description) LIKE '%empower%' THEN 'Empower'
          WHEN LOWER(description) LIKE '%earnin%' AND LOWER(description) NOT LIKE '%learnin%' THEN 'EarnIn'
          WHEN LOWER(description) LIKE '%money%lion%' THEN 'Money Lion'
-         WHEN LOWER(description) LIKE '%varo%' THEN 'Varo'
-         WHEN LOWER(description) LIKE '%cash app%' THEN 'Cash App'
+         --WHEN LOWER(description) LIKE '%varo%' THEN 'Varo'
+         WHEN lower(description) like '%cash app%cash out%' then 'Cash App'
          ELSE NULL
     END
 $$;
@@ -180,5 +180,200 @@ SELECT
     FUNDING_NAME,
     R_AMOUNT,
     T_AMOUNT,
-    SUM(LABEL) over (PARTITION BY USER_ID order by FUNDING_DATE asc rows between unbounded preceding and current row) AS ordernumber
+    SUM(LABEL) over (PARTITION BY USER_ID order by FUNDING_DATE asc LABEL desc rows between unbounded preceding and current row) AS ordernumber
 FROM TMP1
+
+----CHECK TOTAL
+
+SELECT FUNDING_NAME, SUM(T_AMOUNT) AS TOTAL
+FROM SANDBOX.DEV_YYANG.MULTI_DRAW_RAW
+GROUP BY 1
+ORDER BY 1
+
+
+----- THIS TABLE IS NEEDED BY SOME REASON
+
+DROP table IF EXISTS SANDBOX.DEV_YYANG.multi_draw_consecutive;
+CREATE TABLE  SANDBOX.DEV_YYANG.multi_draw_consecutive AS
+WITH DAVE_ADVANCE AS
+(
+    SELECT *
+    FROM SANDBOX.DEV_YYANG.multi_draw_raw
+    WHERE FUNDING_NAME = 'ADVANCE_DAVE'
+)
+SELECT
+    USER_ID,
+    FUNDING_DATE,
+    DATEDIFF('DAY',
+    LAG(FUNDING_DATE, 1) OVER (PARTITION BY USER_ID ORDER BY FUNDING_DATE ASC),
+    FUNDING_DATE) AS CONSECUTIVE_DIFF
+FROM DAVE_ADVANCE
+
+---- THIS TABLE IS NEEDED BY SOME REASON TOO
+
+--CHIME DEDUP
+
+DROP table IF EXISTS SANDBOX.DEV_YYANG.multi_draw_chime_dedup_update;
+CREATE TABLE  SANDBOX.DEV_YYANG.multi_draw_chime_dedup_update AS
+WITH USERSET AS
+(
+    SELECT
+        USER_ID,
+        DATE(PV_TS) AS STARTDATE
+    FROM ANALYTIC_DB.DBT_marts.new_user_attribution
+    WHERE STARTDATE > DATE('2022-12-15')
+),
+chime_raw AS
+(SELECT
+        USERSET.USER_ID AS USER_ID,
+       bal.timestamp::DATE AS balance_date,
+       bal.bank_account_id AS bank_account_id,
+       bal.bank_connection_id AS bank_connection_id,
+       COALESCE(bal."CURR", bal.avail) AS balance,
+       bal.timestamp AS bal_timestamp
+    FROM dave.bank_data_service.balance_log bal
+    JOIN APPLICATION_DB.GOOGLE_CLOUD_MYSQL_DAVE.BANK_ACCOUNT bank_acct
+    ON bal.bank_account_id = bank_acct.id
+    JOIN USERSET
+    ON bal.user_id = USERSET.USER_ID
+    WHERE bank_acct.institution_id in (268940, 104812, 271751)
+    AND bal.timestamp::DATE > DATE('2022-12-15')
+    and balance < -0.001
+    QUALIFY ROW_NUMBER() OVER (PARTITION BY bal.user_id, bank_account_id, balance_date ORDER BY bal_timestamp DESC) = 1
+    ORDER BY USER_ID
+ )
+SELECT * from chime_raw
+
+---For consecutive support table
+
+DROP table IF EXISTS SANDBOX.DEV_YYANG.multi_draw_nonchime_dedup;
+CREATE TABLE  SANDBOX.DEV_YYANG.multi_draw_nonchime_dedup AS
+WITH USERSET AS
+(
+    SELECT
+        USER_ID,
+        DATE(PV_TS) AS STARTDATE
+    FROM ANALYTIC_DB.DBT_marts.new_user_attribution
+    WHERE STARTDATE > DATE('2022-12-15')
+),
+NONCHIME AS
+(
+    SELECT
+        USERSET.USER_ID,
+        transaction_date,
+        DBT.DEV_YANCHUN_PUBLIC.UDF_NONCHIME_COMPETITOR(display_name) AS nonchime_competitor_name,
+        amount
+    FROM DATASTREAM_PRD.DAVE.BANK_TRANSACTION plaid_txn
+    JOIN USERSET
+    ON plaid_txn.USER_ID = USERSET.USER_ID
+    WHERE plaid_txn.user_id IS NOT NULL
+    AND nonchime_competitor_name IS NOT NULL
+    and amount > 0
+    and transaction_date > DATE('2022-12-15')
+),
+NONCHIME_DEDUP AS
+(
+    SELECT DISTINCT * FROM NONCHIME
+),
+NONCHIME_UPDATE AS
+(
+    SELECT
+        USER_ID,
+        transaction_date,
+        nonchime_competitor_name,
+        sum(amount) AS advance_volume
+    FROM NONCHIME_DEDUP
+    GROUP BY USER_ID, transaction_date, nonchime_competitor_name
+)
+SELECT * FROM NONCHIME_UPDATE;
+
+---- FOR CONSECUTIVE
+
+DROP table IF EXISTS SANDBOX.DEV_YYANG.multi_draw_consecutive;
+CREATE TABLE  SANDBOX.DEV_YYANG.multi_draw_consecutive as
+WITH USERSET AS
+(
+    SELECT
+        USER_ID,
+        DATE(PV_TS) AS STARTDATE
+    FROM ANALYTIC_DB.DBT_marts.new_user_attribution
+    WHERE STARTDATE > DATE('2022-12-15')
+),
+chime_raw as
+(
+    select * from SANDBOX.DEV_YYANG.multi_draw_chime_dedup_update
+),
+nonchime_raw as
+(
+    select * from SANDBOX.DEV_YYANG.multi_draw_nonchime_dedup
+),
+ADVANCE AS
+(
+    SELECT
+        USERSET.USER_ID AS USER_ID,
+        DISBURSEMENT_DS_PST AS FUNDING_DATE,
+        advance_id,
+        MAX_APPROVED_AMOUNT AS R_AMOUNT,
+        TAKEN_AMOUNT AS T_AMOUNT,
+        LAG(FUNDING_DATE, -1) OVER (PARTITION BY USERSET.USER_ID ORDER BY FUNDING_DATE) as next_funding_date,
+        LAG(t_amount, -1) OVER (PARTITION BY USERSET.USER_ID ORDER BY FUNDING_DATE) as next_funding_amount
+    from ANALYTIC_DB.DBT_MARTS.disbursements disburse
+    join USERSET
+    ON disburse.USER_ID = USERSET.USER_ID
+    WHERE DISBURSEMENT_DS_PST > DATE('2022-12-15') AND PRODUCT='Extra Cash'
+),
+advance_update as
+(
+    select * from advance where DATEDIFF(DAY, FUNDING_DATE, next_funding_date) <= 28
+
+),
+chime as
+(
+    select
+        a.user_id,
+        a.funding_date,
+        a.next_funding_date,
+        a.advance_id,
+        a.next_funding_amount,
+        a.r_amount,
+        a.t_amount,
+        max(-1 * b.balance) as chime_amount,
+        max(1) as chime_count
+    from advance_update a
+    left join chime_raw b
+    on a.user_id = b.user_id
+    where b.balance_date between a.funding_date and a.next_funding_date
+    group by 1,2,3,4,5,6,7
+),
+nonchime as
+(
+    select
+        a.user_id,
+        a.funding_date,
+        a.next_funding_date,
+        a.advance_id,
+        a.next_funding_amount,
+        a.r_amount,
+        a.t_amount,
+        sum(advance_volume) as nonchime_amount,
+        count(transaction_date) as nonchime_count
+    from advance_update a
+    left join nonchime_raw b
+    on a.user_id = b.user_id
+    where b.transaction_date between a.funding_date and a.next_funding_date
+    group by 1,2,3,4,5,6,7
+)
+select
+    COALESCE(a.user_id, b.user_id) as user_id,
+    COALESCE(a.funding_date, b.funding_date) as funding_date,
+    COALESCE(a.next_funding_date, b.next_funding_date) as next_funding_date,
+    COALESCE(a.advance_id, b.advance_id) as advance_id,
+    COALESCE(a.next_funding_amount, b.next_funding_amount) as next_funding_amount,
+    COALESCE(a.r_amount, b.r_amount) as r_amount,
+    COALESCE(a.t_amount, b.t_amount) as t_amount,
+    case when a.chime_count is null then 0 else a.chime_count end as chime_count,
+    case when a.chime_amount is null then 0 else a.chime_amount end as chime_amount,
+    case when b.nonchime_count is null then 0 else b.nonchime_count end as nonchime_count,
+    case when b.nonchime_amount is null then 0 else b.nonchime_amount end as nonchime_amount
+from chime a full join nonchime b
+on a.user_id  = b.user_id and a.advance_id = b.advance_id
